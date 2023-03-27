08afb8bb176bf312d31b0aed237458dc902043b7
maritza1013 <maritzamae13@gmail.com>
2023-03-27 12:22:35 -0500

    aplicando JavaScript al proyecto

---
+++ js/reloj.js
@@ -0,0 +1,23 @@
+const mostrarReloj = () =>{
+    let fecha = new Date();
+    let hr = formatoHora(fecha.getHours());
+    let min = formatoHora( fecha.getMinutes());
+    let seg = formatoHora(fecha.getSeconds());
+    document.getElementById('hora').innerHTML = `${hr}:${min}:${seg}`
+
+    const meses = ['ene','feb','mar','abr','may','jun','jul','ago','sep','oct','nov','dic']
+    const dias = ['dom','lun','mar','mie','jue','vie','sab']
+    let diaSemana = dias[fecha.getDay()]
+    let dia = fecha.getDate()
+    let mes = meses [fecha.getMonth()]
+    let textoFecha = `${diaSemana},${dia} ${mes}`
+    document.getElementById('fecha').innerHTML = textoFecha
+
+}
+
+const formatoHora = (hora) => {
+    if(hora < 10)
+    hora = '0'+ hora
+    return hora
+}
+setInterval(mostrarReloj, 1000)
